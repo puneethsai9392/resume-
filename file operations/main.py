e896a8aae984353ece2998c37266fbd66e90531e
puneethsai <162569603+puneethsai9392@users.noreply.github.com>
2026-08-03 00:25:20 +0530

    Add files via upload

---
+++ file operations/main.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+#os.mkdir('names')
+#s.rmdir('names')
+#os.makedirs('images/videos')
+#shutil.rmtree('images/videos')
+'''os.mkdir('image')
+path=os.path.join('image','names.txt')
+with open(path,'w+') as f:
+    f.write('Hello World')
+    f.seek(0)
+    print(f.read())'''
+
